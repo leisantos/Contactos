d2e4a74cd4f0aeb5a5d7e1db37447630d77aa9ce
Leidye Santos <enaleidysabtosramirez0@gmail.com>
2021-10-06 21:45:22 -0600

    Agregar archivos de proyecto.

---
+++ Contactos/Contacto.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contactos
+{
+    class Contacto
+    {
+
+        private int contactoId;
+        private string nombre;
+        private string apellido;
+        private int numeroTelefono;
+        private string direccion;
+
+
+        public void setcontactoId(int ContactoId)
+        {
+            contactoId = ContactoId;
+        }
+
+        public int getcontactoId()
+        {
+            return contactoId;
+        }
+
+        public void setnombre(string Nombre)
+        {
+            nombre = Nombre;
+        }
+
+        public string getnombre()
+        {
+            return nombre;
+        }
+
+        public void setapellido(string Apellido)
+        {
+            apellido = Apellido;
+        }
+
+        public string getapellido()
+        {
+            return apellido;
+        }
+
+        public void setnumeroTelefono(int NumeroTelefono)
+        {
+            numeroTelefono = NumeroTelefono;
+        }
+
+        public int getnumeroTelefono()
+        {
+            return numeroTelefono;
+        }
+
+        public void setdireccion(string Direccion)
+        {
+            direccion = Direccion;
+        }
+
+        public string getdireccion()
+        {
+            return direccion;
+        }
+    }
+}
---
+++ Contactos/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Contactos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dgContacto.Columns.Add("contactoId", "ID");
+            dgContacto.Columns.Add("nombre", "Nombre");
+            dgContacto.Columns.Add("apellido", "Apellido");
+            dgContacto.Columns.Add("numeroTelefono", "Telefono");
+            dgContacto.Columns.Add("direccion", "Direccion");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+
+            Contacto contacto = new Contacto();
+
+            contacto.setcontactoId(Convert.ToInt32(txtcontactoId.Text));
+            contacto.setnombre(txtnombre.Text);
+            contacto.setapellido(txtapellido.Text);
+            contacto.setnumeroTelefono(Convert.ToInt32(txtnumeroTelefono.Text));
+            contacto.setdireccion(txtdireccion.Text);
+
+            dgContacto.Rows.Add(contacto.getcontactoId(), contacto.getnombre(), contacto.getapellido(), contacto.getnumeroTelefono(), contacto.getdireccion());
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+    }
+}
